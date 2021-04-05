20b061d1021d9e42c8f3c11c5a3f4e44b609b4ef
kra5alllez <kra5alllez@gmail.com>
2021-04-05 21:07:23 +0300

    changes

--- Module_3_Task_6_Vasylchenko/Module_3_Task_6_Vasylchenko/Services/FileService.cs
+++ Module_3_Task_6_Vasylchenko/Module_3_Task_6_Vasylchenko/Services/FileService.cs
@@ -11,10 +11,11 @@ namespace Module_3_Task_6_Vasylchenko.Services
 {
     public class FileService : IFileService
     {
+        private const string Name = "Logger";
+        private readonly string _path;
         private IFileServiceConfig _file;
         private SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
         private LoggerConfig _loggerConfig;
-        private Model _model;
         private BackupService _bachUp = new BackupService();
         private int _backupNumber;
 
@@ -24,21 +25,23 @@ namespace Module_3_Task_6_Vasylchenko.Services
             _file = new FileServiceConfig();
             InitAsync().GetAwaiter().GetResult();
             BackUp += _bachUp.SimpleWriteAsync;
+            _path = $@"{_loggerConfig.DirectoryPath}{Name}{_loggerConfig.FileExtension}";
         }
 
         public event Func<string, string, Task> BackUp;
+        public string PathWrite { get; init; }
 
         public async Task FileSeveAsync(string text)
         {
             await _semaphoreSlim.WaitAsync();
-            using (StreamWriter streamWriter = new StreamWriter(_model.PathWrite, true, System.Text.Encoding.Default))
+            using (StreamWriter streamWriter = new StreamWriter(_path, true, System.Text.Encoding.Default))
             {
                 await streamWriter.WriteLineAsync(text);
             }
 
             if (_backupNumber % _loggerConfig.ConfigurableNumber == 0)
             {
-                await BackUp($"{_loggerConfig.BackUpPath}{DateTime.UtcNow.ToString(_loggerConfig.TimeFormat)}-{_backupNumber}{_loggerConfig.FileExtension}", _model.PathWrite);
+                await BackUp($"{_loggerConfig.BackUpPath}{DateTime.UtcNow.ToString(_loggerConfig.TimeFormat)}-{_backupNumber}{_loggerConfig.FileExtension}", _path);
             }
 
             _backupNumber++;
@@ -48,8 +51,6 @@ namespace Module_3_Task_6_Vasylchenko.Services
         private async Task InitAsync()
         {
             _loggerConfig = await _file.JsonAsync();
-            var s = $@"{_loggerConfig.DirectoryPath}{DateTime.UtcNow.ToString(_loggerConfig.TimeFormat)}{_loggerConfig.FileExtension}";
-            _model = new Model { PathWrite = s };
         }
     }
 }
